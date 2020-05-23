cbf26a1d20038194613e4919d998142ab641502d
Heyrim <heyrim@gmail.com>
2020-05-22 22:27:47 -0700

    Add files via upload

--- src/BPG_Gradient_newfigure.m
+++ src/BPG_Gradient_newfigure.m
@@ -1,170 +1,165 @@
 % BIDIRECTOINAL PATH GENERATION
 
-%% _________________
+%%_________________
 %Using 5 voxel kernerl blur orientation and coherency map 
 
-%clear all
-directory = pwd;
+%% load data 
+% clear all
+% directory = pwd;
+load( 'NSC_atlas_Vikram/180slice' );
 
-load( 'NSC_atlas_Vikram/180slice' ); 
+eigen_map = EV_slice; 
+coh_map   = FA_slice; % fractional anisotropy 
+ori_map = atand(squeeze( EV_slice(:,:,2) )./squeeze( EV_slice(:,:,1) )); 
+clear EV_slice FA_slice 
 
-% WM = imread(strcat(pwd,'/WM_M43.tif'));
-% WM = logical(WM.*(WM>10));                                                                              % Threshold image to remove background
-% ori_map = 180*imread(strcat(pwd,'/Orientation_5vox_blur.tif'))/pi;                                      % Orientation map converted to degrees (-90 to 90 deg)
-% temp = zeros(size(ori_map));
-% temp(ori_map>0) = ori_map(ori_map>0);                                                                   % positive angles are ascribed to 0-90 degrees
-% temp(ori_map<0) = 180+ori_map(ori_map<0);                                                               % Negative angles are ascribed to 90 - 180 deg
-% ori_map = temp; clear temp;
-
-eval_map = EV_slice; 
-coh_map = FA_slice; % fractional anisotropy 
-d = 5;                                                                                                  % Step size of each simulation step
-coh_limit = 0.1;                                                                                        % Coherency limit for terminating path
+%% parameters 
+set_parameters_Simple()                                                                                      % Coherency limit for terminating path
 
 %% Initialize a matrix of same size as the white matter image and initialize a seed point (inj center) around which the seeds for each simulation are placed
-[X,Y] = meshgrid(1:size(WM,2),1:size(WM,1));
+[X,Y] = meshgrid(1:size(coh_map,2),1:size(coh_map,1));
 inj_center = [400,270]; % in corpus callosum
 seed_ROI = sqrt((X-inj_center(2)).^2 + (Y-inj_center(1)).^2)<10;                                       % All points within 500 pixel distance from inj_center
 seed_ind = find(seed_ROI);                                                                              % Indices of all these points
-n_seeds = 50;                                                                                          % Total number of paths generated
-WM_path = double(WM);  
+n_seeds = 10;                                                                                          % Total number of paths generated
+
 % Storing the coorinate, angle of eigen vector and coherency value for both
 % positive and negative direction paths that start from a seed point
 p = struct('coord1',{},'angle1',[],'coherency1',[],'coord2',{},'angle2',[],'coherency2',[]);             % the three fields 
 
 
-% cancer injection
+
+%% cancer injection
 cancer_center = [160,280]; % near contralateral corpus callosum 
-cancer_param = 20;
-concentration = exp((-(X - cancer_center(2)).^2 - (Y - cancer_center(1)).^2)./cancer_param^2);
+cancer_sd = 20;
+[concentration, cgradX, cgradY] = set_cancer( cancer_center, cancer_sd, X, Y ); 
 
-cgradY = zeros(size(ori_map));
-cgradY(2:end-1,:) = concentration(3:end,:) - concentration(1:end-2,:);
-
-cgradX = zeros(size(ori_map));
-cgradX(:,2:end-1) = concentration(:,3:end) - concentration(:,1:end-2);
+chmtx_limit = 0.1; 
 
 
-% find largest vector magnitude
-L = (cgradX.^2 + cgradY.^2).^(.5);
-maxL = max(max(L));
+%% 
+Tstart = tic; 
+Finaltimestep = 10000; 
 
-% normalize gradient
-cgradX = cgradX./maxL;
-cgradY = cgradY./maxL;
-
-
-% create matrix that holds angle of gradient vectors
-% % cgradAng = zeros(size(ori_map));
-% cgradAng = atan(cgradY./cgradX);
-% cgradAng = 180 * cgradAng / pi;
-
-
-%%
-coord = zeros(1,2,n_seeds);                                                                              % coordinates for each point for all paths generated in simulation
+%% for each seed 
 for seed_loop = 1:n_seeds
-% seed_loop
+
+    %%%% initialize first two steps 
     seed = seed_ind(round(rand*length(seed_ind)));                                                       % Choose a random point index to start the simulation
     [seed(1,1),seed(1,2)] = ind2sub(size(seed_ROI),seed);                                               % Coordinate of this starting point
-    p(seed_loop).coord1{1} = seed;                                                                      % Starting location stored in data matrix
-    p(seed_loop).coord2{1} = seed;
-    ind = sub2ind(size(WM),seed(1,1),seed(1,2));                                                      % Index of this point in the white matter image
+    p(seed_loop).coord1(:,1) = seed';                                                                      % Starting location stored in data matrix
+    ind = sub2ind(size(coh_map),seed(1,1),seed(1,2));                                                      % Index of this point in the white matter image
     % If coherency map data is present then use the angle indicated by the
     % data, else use a random angle
-    if coh_map(ind)
-        p(seed_loop).angle1(1) = -ori_map(ind);                                                         % Starting orientation
-        p(seed_loop).angle2(1) = -ori_map(ind);
-    else
-        p(seed_loop).angle1(1) = -90 + 2*rand*90;                                                       % Random angle generated if no coherency data
-        p(seed_loop).angle2(1) = -90 + 2*rand*90;
-    end
-    
-    
-    path = zeros(size(WM));
-    flag1 = 0;  flag2 = 0;                                                                              % Flag indicating whether the path should continue 
-    
-    
-    for i = 2:4000                                                                                    % Run the loop a large number of steps
+    p(seed_loop).angle1(1) = ori_map(seed(1,1),seed(1,2)); 
 
-        if ~flag1                                                                                       % Positive direction path
+    %%%% second step 
+    i = 2; 
+%     ind1 = sub2ind(size(WM),round(p(seed_loop).coord1(1,i-1)),round(p(seed_loop).coord1(2,i-1)));                                             % Index of previous coordinate point
             
-            seednow = [round(p(seed_loop).coord1{i-1}(1)), round(p(seed_loop).coord1{i-1}(2))]; 
-            ind1 = sub2ind(size(WM),seednow(1),seednow(2));                                             % Index of previous coordinate point
-            if coh_map(ind1)                                                                            % Get Eigen vector angle at the previous coordinate point
-                angle = -ori_map(ind1);
-                p(seed_loop).angle1(i) = angle;
+%     angle = -ori_map(ind1);
+%     p(seed_loop).angle1(i) = angle;            
+%     p(seed_loop).coherency1(i) = coh_map(ind1);
+    
+    eigen_vect = squeeze( eigen_map( round(p(seed_loop).coord1(1,i-1)),round(p(seed_loop).coord1(2,i-1)), : )); 
+    dstep = (rand(1)*2-1)*dmax; %* double(coh_map(ind1));     
+    p(seed_loop).coord1(:,i) = p(seed_loop).coord1(:,i-1) + dstep*eigen_vect;                           % Find next coordinate point at step d along EV direction
+    
+%     path = zeros(size(WM));
+    
+    flag1 = 0; 
+    for i = 3:Finaltimestep                                                                                    % Run the loop a large number of steps            
+        if ~flag1  
+            seedx = round(p(seed_loop).coord1(1,i-1)); 
+            seedy = round(p(seed_loop).coord1(2,i-1)); 
+            
+            ind1 = sub2ind(size(coh_map),seedx,seedy);                                             % Index of previous coordinate point
+            
+            if coh_map(ind1) > coh_limit                                                                           % Get Eigen vector angle at the previous coordinate point
+%                 angle = -ori_map(ind1);
+%                 p(seed_loop).angle1(i) = angle; 
+                eigen_vect = squeeze( eigen_map( seedx, seedy, : ) ); 
+                d = dmax; 
+                
             else
-                angle_sampled = -90+2*rand*90;
-                angle = rem(angle_sampled + p(seed_loop).angle1(i-1),360);                              % Real value of angle if sampled angle + original angle > 360 deg
-                p(seed_loop).angle1(i) = angle_sampled;
-            end
-            
+% %                 angle_sampled = -90+2*rand*90;
+                angle = -45+2*rand*45;
+                if ~isnan(ori_map(seedx, seedy))
+                    angle = rem( angle + ori_map(seedx, seedy) ,360);     
+                end
+                p(seed_loop).angle1(i) = angle;
+                eigen_vect = [cosd(angle), sind(angle)]'; 
+
+                % move with less magnitude if no WM present 
+                d = 0; %dmax/100; 
+                
+            end 
             
             p(seed_loop).coherency1(i) = coh_map(ind1);
-            eigen_vect = [sind(angle),cosd(angle)];                                                     % Calculate the eigen vector (EV) direction
             
-            p(seed_loop).coord1{i} = p(seed_loop).coord1{i-1} - d*eigen_vect;                           % Find next coordinate point at step d along EV direction
             
-            sensitivityMag = 10;
+            %%%%%% uniform[0, 1] 
+%             dstep = rand(1)*dmax ; 
+            %%%%%% beta[1, beta], if beta==1, uniform 
+            dstep = betainv( rand(1), 1, beta4dist )*d ; 
+            dstep = d; 
+            
+            
+           
+            %% model 1 
+            % Find next coordinate point at step d along EV direction 
+            p(seed_loop).coord1(:,i) = p(seed_loop).coord1(:,i-1) ... 
+                - sign(dot( p(seed_loop).coord1(:,i-2) - p(seed_loop).coord1(:,i-1), eigen_vect ) + eps ) * dstep*eigen_vect;                           % Find next coordinate point at step d along EV direction
+            % let it move away from the direction it came from. 
+            
+            p(seed_loop).coord1(:,i) =   p(seed_loop).coord1(:,i) + 0.1*randn(2,1); 
+            
+            %% model 2 
+%             % add chemotaxis with (1-w) weight
+%             w = betainv( rand(1), beta4chmtx, 1 ) ; 
+% 
+%             eigen_vect = - sign(dot( p(seed_loop).coord1(:,i-2) - p(seed_loop).coord1(:,i-1), eigen_vect ) + eps ) * eigen_vect; 
+%             chmtx_vect = [cgradY(seedx,seedy); cgradX(seedx,seedy)]; 
+%             p(seed_loop).coord1(:,i) = p(seed_loop).coord1(:,i-1) ... 
+%                 + dstep*( w*eigen_vect + (1-w)*chmtx_vect );
+            
+            %% model 3 
+            %  move along chemotaxis once it is near by cancer 
+%             if( concentration(seedx,seedy) <= chmtx_limit )
+%                 
+%             p(seed_loop).coord1(:,i) = p(seed_loop).coord1(:,i-1) ... 
+%                 - sign(dot( p(seed_loop).coord1(:,i-2) - p(seed_loop).coord1(:,i-1), eigen_vect ) + eps ) * dstep*eigen_vect;   
+% 
+%             else
+%             
+%             p(seed_loop).coord1(1,i) = p(seed_loop).coord1(1,i) + dstep * cgradY(seedx,seedy);
+%             p(seed_loop).coord1(2,i) = p(seed_loop).coord1(2,i) + dstep * cgradX(seedx,seedy);
+% 
+%             end 
+            
+            if( mod( i, 100 ) == 0 )
+                i;
+            end
 
-            p(seed_loop).coord1{i}(1) = p(seed_loop).coord1{i}(1) + sensitivityMag * cgradY(seednow(1,1),seednow(1,2));
-            p(seed_loop).coord1{i}(2) = p(seed_loop).coord1{i}(2) + sensitivityMag * cgradX(seednow(1,1),seednow(1,2));
-
-            %coord(i,1:2,seed_loop) = coord(i-1,1:2,seed_loop) + d*eigen_vect;
-%             if ~all(round(p(seed_loop).coord1{i})>0)
-%                 pause
-%             end
-            if( p(seed_loop).coord1{i}(1) > 5000 ) 
-                disp('what') 
-            end 
-                
-                
-% the path is out of bounds or path a loop has formed then don't proceed and change flag
-            if all(p(seed_loop).coord1{i}-size(WM)<=0)  && all(round(p(seed_loop).coord1{i})>0)  && ~path(round(p(seed_loop).coord1{i}(1)),round(p(seed_loop).coord1{i}(2)))                  
-                xpts = linspace(p(seed_loop).coord1{i-1}(1),p(seed_loop).coord1{i}(1),d);
-                ypts = linspace(p(seed_loop).coord1{i-1}(2),p(seed_loop).coord1{i}(2),d);
-                path(sub2ind(size(WM),round(xpts),round(ypts))) = 200;
+% % the path is out of bounds or hit max steps then don't proceed and change flag
+%             
+% % ------------------------------------------                
+            seednow = round(p(seed_loop).coord1(:,i)); 
+            if all( seednow' - size(coh_map)<=-1)  && all( seednow' >1) && ~isnan(ori_map(seednow(1),seednow(2)))
+% %                 % to interpolate the path to integers 
+% %                 xpts = round( linspace(p(seed_loop).coord1(1,i-1),p(seed_loop).coord1(1,i),10) );
+% %                 ypts = round( linspace(p(seed_loop).coord1(2,i-1),p(seed_loop).coord1(2,i),10) );                
+%                 
             else
                 flag1 = 1;
             end
+% % ------------------------------------------                
         end
-        
-        
-        % Same algorithm for generating the negative direction path. Both
-        % should be combined into one loop... for later
-        
-        if ~flag2
-
-            seednow = [round(p(seed_loop).coord2{i-1}(1)), round(p(seed_loop).coord2{i-1}(2))]; 
-            ind2 = sub2ind(size(WM),seednow(1),seednow(2));   
-            if coh_map(ind2)
-                angle = -ori_map(ind2);
-                p(seed_loop).angle2(i) = angle;
-            else
-                angle_sampled = -90+2*rand*90;
-                angle = rem(angle_sampled + p(seed_loop).angle2(i-1),360);
-                p(seed_loop).angle2(i) = angle_sampled;
-            end
-            p(seed_loop).coherency2(i) = coh_map(ind2);
-            eigen_vect = [sind(angle),cosd(angle)];
-            p(seed_loop).coord2{i} = p(seed_loop).coord2{i-1} + d*eigen_vect;
-            %coord(i,1:2,seed_loop) = coord(i-1,1:2,seed_loop) + d*eigen_vect;
-            if all(p(seed_loop).coord2{i}-size(WM)<=0)  && all(round(p(seed_loop).coord2{i})>0)  && ~path(round(p(seed_loop).coord2{i}(1)),round(p(seed_loop).coord2{i}(2)))                  % the path is out of bounds or path a loop has formed
-                xpts = linspace(p(seed_loop).coord2{i-1}(1),p(seed_loop).coord2{i}(1),d);
-                ypts = linspace(p(seed_loop).coord2{i-1}(2),p(seed_loop).coord2{i}(2),d);
-                path(sub2ind(size(WM),round(xpts),round(ypts))) = 200;
-            else
-                flag2 = 1;
-            end
-
-        end
-        
-        
     end
-%    % Initialize the path indices on the path image with each path having
-%    a different index. This is just another way for plotting on same plot.
-%    WM_path(logical(path)) = .1*seed_loop;
+        
 end
+
+toc( Tstart ); 
 
 %% Plotting the paths
 % figure; imagesc(WM_path); axis equal;
@@ -172,19 +167,71 @@ end
 %edit_indices = [109,93,303,416,133,112,378];
 
 % get the coordinate data from each coord1 and coord2 data and plot them.
-X_coords = zeros(1,n_seeds);
-Y_coords = X_coords;
+xfinal = zeros(1,n_seeds); 
+yfinal = zeros(1,n_seeds);
+figure; imagesc(coh_map);   colormap gray;  axis equal;  hold on;
 for i = 1:n_seeds
-    coord_data1 = cell2mat(p(i).coord1);
-    l1 = length(coord_data1);
-    X_coords(1:l1/2,i) = coord_data1(1:2:l1)';    
-    Y_coords(1:l1/2,i) = coord_data1(2:2:l1)'; 
-    coord_data2 = cell2mat(p(i).coord2);
-    l2 = length(coord_data2);
-    X_coords(l1/2+1:(l1+l2)/2,i) = coord_data2(1:2:l2)';    
-    Y_coords(l1/2+1:(l1+l2)/2,i) = coord_data2(2:2:l2)'; 
+    
+    coord_data1 = p(i).coord1; 
+    l1 = size(coord_data1,2);
+    if( l1 <= 5000 )
+    X_coords = coord_data1(1, :);   
+    Y_coords = coord_data1(2, :); 
+    else
+    X_coords = coord_data1(1,1:ceil(l1/5000):l1)';    
+    Y_coords = coord_data1(2,1:ceil(l1/5000):l1)'; 
+    end         
+    plot(Y_coords,X_coords,'.', 'markersize', 2);  
+    
+    xfinal(i) = coord_data1(1, end); 
+    yfinal(i) = coord_data1(2, end); 
 end
-figure; imagesc(coh_map);   colormap gray;  hold on;
-plot(Y_coords,X_coords,'.'); axis equal
+% %%% to plot final location 
+plot(yfinal, xfinal,'ow', 'markersize', 4, 'markerfacecolor', 'w'); 
 
+nprint = 0; 
+if( nprint ) 
+
+    % %%% to plot distribution each time  
+    for i = 1:n_seeds 
+        if( size( p(i).coord1, 2 ) < Finaltimestep ) 
+            ind = size( p(i).coord1, 2 ); 
+            p(i).coord1(1,(ind+1):Finaltimestep) = p(i).coord1(1,ind); 
+            p(i).coord1(2,(ind+1):Finaltimestep) = p(i).coord1(2,ind);         
+        end
+        coord(:,:,i) = p(i).coord1';         
+    end
+    Tstep = floor( Finaltimestep/5 ); 
+    figure; 
+    for n = 1:5 
+        subplot( 1, 5, n ); imagesc(coh_map);   colormap gray;  hold on; 
+        plot(squeeze( coord(Tstep*n,2,:)), squeeze( coord(Tstep*n,1,:)),'ow', 'markersize', 4, 'markerfacecolor', 'w'); 
+    end 
+    
+    
+
+end 
+
+disp( 'done' ); 
+
+
+function [concentration, cgradX, cgradY] = set_cancer( cancer_center, cancer_sd, X, Y ) 
+
+    concentration = exp((-(X - cancer_center(2)).^2 - (Y - cancer_center(1)).^2)./cancer_sd^2);
+
+    cgradY = zeros(size(X));
+    cgradY(2:end-1,:) = concentration(3:end,:) - concentration(1:end-2,:);
+
+    cgradX = zeros(size(X));
+    cgradX(:,2:end-1) = concentration(:,3:end) - concentration(:,1:end-2);
+
+    % find largest vector magnitude
+    L = (cgradX.^2 + cgradY.^2).^(.5);
+    maxL = max(max(L));
+
+    % normalize gradient
+    cgradX = cgradX./maxL;
+    cgradY = cgradY./maxL;
+
+end 
 
