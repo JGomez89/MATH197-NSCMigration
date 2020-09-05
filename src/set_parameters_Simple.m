846cfaf951311394e94141627ab7b31208bbf8c0
Nathanael Holmes <43829916+IllumiNate411@users.noreply.github.com>
2020-09-05 12:24:30 -0700

    Add files via upload

--- src/set_parameters_Simple.m
+++ src/set_parameters_Simple.m
@@ -2,8 +2,8 @@ global modelType timeStep cancer_center chemo_sensitivity dmax dstochastic alpha
 
 modelNum = 2;
 
-chemo_sensitivity = 10;
-has_cancer = 0;
+chemo_sensitivity = 6;
+has_cancer = 1;
 dstochastic = 8; 
 alpha4chmtx = 1;
 beta4dist = 1;
@@ -11,6 +11,9 @@ cancer_radius = 1000;
 coh_limit = 0.1;  
 chmtx_limit = 0.15;
 % timeStep = 588;
+
+d_w = 5;
+d_g = 5/dstochastic;
 
 FolderName1 = '\Figures\CancerNA\'; %Save plots to this folder (Must pre-exist)
 FolderName2 = '\Figures\Cancer\'; %Save plots to this folder (Must pre-exist)
